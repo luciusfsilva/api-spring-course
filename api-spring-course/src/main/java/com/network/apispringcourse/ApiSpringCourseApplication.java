150f5117ccae5436f5ea3d740c35a76994ad3e3e
luciusfsilva <lucianoferreiraofficial@gmail.com>
2022-02-04 15:05:25 -0300

    Primeiro commit

---
+++ api-spring-course/src/main/java/com/network/apispringcourse/ApiSpringCourseApplication.java
@@ -0,0 +1,13 @@
+package com.network.apispringcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringCourseApplication.class, args);
+	}
+
+}
